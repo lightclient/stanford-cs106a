ecf835382be2725af3c14595a3cd5d27f0b09c2c
Matt Garnett <matt.garnett@me.com>
2020-04-19 13:08:41 -0600

    complete collect newspaper karel

--- assignment-01/CollectNewspaperKarel.py
+++ assignment-01/CollectNewspaperKarel.py
@@ -13,12 +13,67 @@ to its initial position in the upper left corner of the house.
 
 def main():
     """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
+    Guide Karel to the newspaper outside and return them to their starting
+    position.
+
+    The strategy here is to traverse around the building until a door is
+    found. After find the door, Karel will exit and collect the newspaper.
+    Karel will then move back inside until they hit a wall. After hitting
+    the wall they will traverse around the bulding again until they are
+    in the upper left corner.
     """
-    pass
+    
+    # find door
+    while not top_is_clear() and not beepers_present():
+        if front_is_clear():
+            move()
+        else:
+            turn_right()
+            move()
+
+    # turn to face out the door
+    turn_left()
+
+    # pick up paper
+    while not beepers_present():
+        move()
+        
+    pick_beeper()
+
+
+    # go back inside
+    turn_around()
+    while front_is_clear():
+        move()
+
+    turn_right()
+
+    # traverse inside until in the upper left corner
+    while front_is_clear() or not facing_north():
+        if front_is_clear():
+            move()
+        else:
+            turn_right()
+            move()
+
+    turn_right()
+
+
+def top_is_clear():
+    turn_left()
+    clear = front_is_clear()
+    turn_right()
+    return clear
+
+
+def turn_around():
+    for _ in range(2):
+        turn_left()
+
+
+def turn_right():
+    for _ in range(3):
+        turn_left()
 
 
 # There is no need to edit code beyond this point
