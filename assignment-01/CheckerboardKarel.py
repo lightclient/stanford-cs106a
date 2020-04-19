c5d82d43bb54f14402c05112c8fdd7e0e991868e
Matt Garnett <matt.garnett@me.com>
2020-04-19 13:32:34 -0600

    complete checkerboard karel

--- assignment-01/CheckerboardKarel.py
+++ assignment-01/CheckerboardKarel.py
@@ -12,13 +12,59 @@ sample worlds supplied in the starter folder.
 
 def main():
     """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
-    pass
+    Draw a checkerboard with beepers.
 
+    The strategy Karel uses is to construct each row and return to their
+    starting position for the row before determining if they can continue to
+    a higher row. If a beeper is in the first position in the row, Karel will
+    move forward once to offset the next row -- thereby creating the checkered
+    pattern.
+    """
+
+    turn_left()
+
+    while front_is_clear():
+        turn_right()
+
+        place_checkers()
+        turn_around()
+
+        while front_is_clear():
+            move()
+
+        turn_around()
+
+        # make sure to offset checkers
+        if beepers_present():
+            move()
+
+        turn_left()
+        move()
+
+    # place checkers on top row
+    turn_right()
+    place_checkers()
+
+
+def place_checkers():
+    put_beeper()
+
+    while front_is_clear():
+        move()
+
+        if front_is_clear():
+            move()
+            put_beeper()
+
+
+def turn_right():
+    for _ in range(3):
+        turn_left()
+
+
+def turn_around():
+    for _ in range(2):
+        turn_left()
 
 # There is no need to edit code beyond this point
 
