0432066141dfa6629237010956c159290fb07377
Matt Garnett <matt.garnett@me.com>
2020-04-30 23:15:33 -0600

    completed part 2

--- assignment-02/khansole_academy.py
+++ assignment-02/khansole_academy.py
@@ -6,15 +6,28 @@ Add your comments here.
 
 import random
 
+MIN_RANDOM = 10
+MAX_RANDOM = 99
+STREAK_TO_WIN = 3
 
 def main():
-    """
-    You should write your code for this program in this function.
-    Make sure to delete the 'pass' line before starting to write
-    your own code. You should also delete this comment and replace
-    it with a better, more descriptive one.
-    """
-    pass
+    streak = 0
+
+    while streak < STREAK_TO_WIN:
+        x = random.randint(MIN_RANDOM, MAX_RANDOM)
+        y = random.randint(MIN_RANDOM, MAX_RANDOM)
+
+        print("What is {} + {}? ".format(x, y))
+        a = input("Your answer: ") 
+
+        if int(a) == x + y:
+            streak += 1
+            print("Correct! You've gotten " + str(streak) + " in a row.")
+        else:
+            streak = 0
+            print("Incorrect. The expected answer is " + str(x + y))
+
+    print("Congratulation! You mastered addition.")
 
 
 # This provided line is required at the end of a Python file
