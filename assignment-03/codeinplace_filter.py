37e377fc9ec4e0afc1830f5b6d0f9cb6c8fc3205
Matt Garnett <matt.garnett@me.com>
2020-05-08 10:39:43 -0600

    completed codeinplace_filter

--- assignment-03/codeinplace_filter.py
+++ assignment-03/codeinplace_filter.py
@@ -17,7 +17,10 @@ def main():
     image.show()
 
     # Apply the filter
-    # TODO: your code here
+    for pixel in image:
+        pixel.red = pixel.red * 1.5
+        pixel.green = pixel.green * 0.7
+        pixel.blue = pixel.blue * 1.5
 
     # Show the image after the transform
     image.show()
